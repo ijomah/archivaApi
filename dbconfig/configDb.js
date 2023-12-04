192ab6946ab227f3591109df9c7df5b4e55e894e
ijomah jacob <ijomahifinwa@gmail.com>
2023-12-04 12:20:13 -0800

    db config pointing staging

--- dbconfig/configDb.js
+++ dbconfig/configDb.js
@@ -2,6 +2,6 @@ const knex = require("knex");
 
 const knexConfig = require("../knexfile");
 
-const db = knex(knexConfig.development);
+const db = knex(knexConfig.staging);
 
 module.exports = db
--- knexfile.js
+++ knexfile.js
@@ -5,39 +5,39 @@
  */
 module.exports = {
 
-  development: {
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      database : 'postgres',
-      user :     'postgres',
-      password : 'digits1985',
-      charset : 'utf8' 
-    },
-    migrations: {
-      directory: './dbconfig/migration'
-    }
-  },
-
-  // staging: {
+  // development: {
   //   client: 'pg',
   //   connection: {
-  //     connectionString: process.env.DATABASE_URL,
-  //     ssl: {rejectUnauthorized: false},
-  //     host: process.env.DATABASE_HOST,
-  //     port: process.env.PORT || 5432,
-  //     database: process.env.DATABASE_DB,
-  //     user:     process.env.DATABASE_USER,
-  //     password: process.env.DATABASE_PW
+  //     host : '127.0.0.1',
+  //     database : 'postgres',
+  //     user :     'postgres',
+  //     password : 'digits1985',
+  //     charset : 'utf8' 
   //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   // migrations: {
-  //   //   tableName: 'knex_migrations'
-  //   // }
+  //   migrations: {
+  //     directory: './dbconfig/migration'
+  //   }
   // },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {rejectUnauthorized: false},
+      host: process.env.DATABASE_HOST,
+      port: process.env.PORT || 5432,
+      database: process.env.DATABASE_DB,
+      user:     process.env.DATABASE_USER,
+      password: process.env.DATABASE_PW
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
 
   // production: {
   //   client: 'postgresql',
