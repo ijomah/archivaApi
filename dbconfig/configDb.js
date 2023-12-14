cf7a332c56d5d16bc74e75280b4f7b8065b4aaab
ijomah jacob <ijomahifinwa@gmail.com>
2023-12-14 14:24:28 -0800

    now on staging

--- dbconfig/configDb.js
+++ dbconfig/configDb.js
@@ -2,7 +2,7 @@ const knex = require("knex");
 
 const knexConfig = require("../knexfile");
 
-const db = knex(knexConfig.development);
-// const db = knex(knexConfig.staging);
+// const db = knex(knexConfig.development);
+const db = knex(knexConfig.staging);
 
 module.exports = db
